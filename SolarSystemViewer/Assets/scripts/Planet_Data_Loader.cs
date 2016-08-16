7055bbff5975bb17ea9ca6aa4710024971d92222
Dr. James H. Money <james.money@inl.gov>
2016-08-16 11:05:57 -0600

    LIVE-14 Update viewer
    Update viewer to have trail for moving planets. Removed condition to get max frame. Added code to dynamically adjust skipped timesteps based on framerate.

--- SolarSystemViewer/Assets/scripts/Planet_Data_Loader.cs
+++ SolarSystemViewer/Assets/scripts/Planet_Data_Loader.cs
@@ -5,7 +5,8 @@ using System.IO;
 using System.Collections.Generic;
 using System;
 
-public class Planet_Data_Loader : MonoBehaviour {
+public class Planet_Data_Loader : MonoBehaviour
+{
 
 	List<StreamReader> reader;
 	List<GameObject> bodies;
@@ -24,20 +25,38 @@ public class Planet_Data_Loader : MonoBehaviour {
 
 	// variables for keeping track of timestep
 	private int currentFrame = 0; // index for keeping track of the current data timestep to use
-	private int maxFrames = 0; // the max number of frames contained in the data
+	//private int maxFrames = 0; // the max number of frames contained in the data
 
 	// camera control variables
 	private float yaw = 0.0f;
 	private float pitch = 0.0f;
 	private float moveSpeed = 0.1f;
 
+	private double h = Math.Pow(0.5, 14);
+	private double steps_per_second = 0.0;
+	private int frames_per_second = 60;
+	private int skip_count = 1;
+
+	int m_frameCounter = 0;
+	float m_timeCounter = 0.0f;
+	float m_lastFramerate = 0.0f;
+	public float m_refreshTime = 0.5f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		init();
 	}
 	
-	void init() {
+	void init()
+	{
+
+		steps_per_second = 1.0 / h;
+		print ("steps = " + steps_per_second);
+		print ("frames per second = " + frames_per_second);
+		skip_count = (int) (steps_per_second / frames_per_second);
+		print("skipping " + skip_count + " data points per frame");
+
 		reader = new List<StreamReader>();
 		// init list of bodies
 		bodies = new List<GameObject>();
@@ -51,75 +70,119 @@ public class Planet_Data_Loader : MonoBehaviour {
 		bodies.Add(uranus);
 		bodies.Add(neptune);
 		bodies.Add(pluto);
+
+		foreach(GameObject body in bodies)
+		{
+			LineRenderer lr = body.GetComponent<LineRenderer> ();
+			if (lr != null)
+			{
+				lr.SetVertexCount (0);
+			}
+
+		}
+
 		// init readers for reading the data files
-		for (int i = 0; i < bodies.Count; i++) {
+		for (int i = 0; i < bodies.Count; i++)
+		{
 			string filename = "Assets/nbody_data/coord" + (i+1);
 			StreamReader newReader = new StreamReader(filename, Encoding.Default);
 			reader.Add(newReader);
 		}
 		// scale the planets properly
 		scalePlanets();
-		// get timestep info
+
 		currentFrame = 0;
-		maxFrames = getMaxFrames("Assets/nbody_data/coord1");
-		print (maxFrames);
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		readNextTimeStep();
+
+		if( m_timeCounter < m_refreshTime )
+		{
+			m_timeCounter += Time.deltaTime;
+			m_frameCounter++;
+		}
+		else
+		{
+			//This code will break if you set your m_refreshTime to 0, which makes no sense.
+			m_lastFramerate = (float)m_frameCounter/m_timeCounter;
+			frames_per_second = (int)m_lastFramerate;
+			//print ("frames per second = " + frames_per_second);
+			skip_count = (int) (steps_per_second / frames_per_second);
+			//print("skipping " + skip_count + " data points per frame");
+
+			m_frameCounter = 0;
+			m_timeCounter = 0.0f;
+		}
+		currentFrame++;
+
+		try
+		{
+			readNextTimeStep();
+		}
+		catch(Exception e)
+		{
+			print (e.Message);
+			print ("Simulation reinitialized because end of data was likely reached.");
+			cleanup ();
+			init ();
+		}
 		handleCameraInput();
 	}
 
 	// properly deletes objects
-	private void cleanup() {
+	private void cleanup()
+	{
 		for (int i = 0; i < reader.Count; i++) {
 			reader[i].Close();
 		}
 	}
-
-	// returns the max number of timesteps in the simulation
-	private int getMaxFrames(string fileName) {
-		int count = 0;
-		StreamReader reader = new StreamReader(fileName, Encoding.Default);
-		while (reader.ReadLine() != null)
-			count ++;
-		return count;
-	}
-
-	// updates the simulation based on the next timestep
-	private void readNextTimeStep() {
-		try {
-			string line;
-			for (int i = 0; i < bodies.Count; i++) {
-				for (int j = 0; j < 15; j++) // this for loop is for skipping a given number of data entries
-					getNextPosition(reader[i]);
-				line = getNextPosition(reader[i]);
-				if (line != null) {
-					string[] values = line.Split(' ');
-					bodies[i].transform.position = new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]));
-				}
-			}
-		} catch (Exception e) {
-			print (e.Message);
-			print ("Simulation reinitialized because end of data was likely reached.");
-		}
 		
+	// updates the simulation based on the next timestep
+	private void readNextTimeStep()
+	{
+		string line;
+		for (int i = 0; i < bodies.Count; i++)
+		{
+			for (int j = 0; j < (skip_count-1); j++) // this for loop is for skipping a given number of data entries
+				getNextPosition(reader[i]);
+			
+			line = getNextPosition(reader[i]);
+			if (line == null)
+				throw new EndOfStreamException("End of file reached");
+			
+			if (line != null)
+			{
+				string[] values = line.Split(' ');
+				if (values.Length < 3)
+					throw new FormatException ("Invalid format for coordinate");
+				
+				bodies[i].transform.position = new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]));
+
+				Planet planet = bodies[i].GetComponent<Planet>();
+				if (planet!=null)
+				{
+					bodies [i].GetComponent<LineRenderer> ().SetVertexCount (currentFrame);
+					bodies[i].GetComponent<LineRenderer>().SetPosition(currentFrame-1,bodies[i].transform.position);
+				}
+
+			}
+
+		}
+
 	}
 
-	private String getNextPosition(StreamReader reader) {
-		if (currentFrame <= maxFrames) {
-			string line = reader.ReadLine();
-			currentFrame++;
-			return line;
-		} else {
-			init(); // reset simulation
-			throw new Exception();
-		}
+	private String getNextPosition(StreamReader reader)
+	{
+			return reader.ReadLine();
 	}
 
 	// handle wasd and mouse inputs for moving in the simulation
-	private void handleCameraInput() {
+	private void handleCameraInput()
+	{
+		if (Input.GetKey (KeyCode.Escape))
+			Application.Quit ();
 		if (Input.GetKey("w"))
 			transform.Translate(0, 0, moveSpeed, Space.Self);
 		if (Input.GetKey("a"))
@@ -129,7 +192,8 @@ public class Planet_Data_Loader : MonoBehaviour {
 		if (Input.GetKey("d"))
 			transform.Translate(moveSpeed, 0, 0, Space.Self);
 		// Update camera rotations 
-		if (Input.GetMouseButton(0)) { // left mouse button
+		if (Input.GetMouseButton(0))
+		{ // left mouse button
 			yaw += Input.GetAxis("Mouse X");
 			pitch -= Input.GetAxis("Mouse Y");
 			transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
@@ -137,7 +201,8 @@ public class Planet_Data_Loader : MonoBehaviour {
 	}
 
 	// scales planet models to be accurate sizes relative to the sun
-	private void scalePlanets() {
+	private void scalePlanets()
+	{
 		// NEEDS TO BE IMPLEMENTED PROPERLY
 //		for (int i = 0; i < bodies.Count; i++) {
 //			bodies[i].transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
@@ -146,15 +211,15 @@ public class Planet_Data_Loader : MonoBehaviour {
 		// sun
 		bodies[9].transform.localScale = new Vector3(sunSize, sunSize, sunSize);
 		// mercury
-		bodies[1].transform.localScale = new Vector3(sunSize * 1.0f/277.0f, sunSize * 1.0f/277.0f, sunSize * 1.0f/277.0f);
+		bodies[1].transform.localScale = new Vector3(sunSize * 1.0f/5.0f, sunSize * 1.0f/5.0f, sunSize * 1.0f/5.0f);
 		// venus
-		bodies[2].transform.localScale = new Vector3(sunSize  * 1.0f/113.0f, sunSize  * 1.0f/113.0f, sunSize  * 1.0f/113.0f);
+		bodies[2].transform.localScale = new Vector3(sunSize  * 1.0f/5.0f, sunSize  * 1.0f/5.0f, sunSize  * 1.0f/5.0f);
 		// earth
-		bodies[3].transform.localScale = new Vector3(sunSize  * 1.0f/108.0f, sunSize  * 1.0f/108.0f, sunSize  * 1.0f/108.0f);
+		bodies[3].transform.localScale = new Vector3(sunSize  * 1.0f/5.0f, sunSize  * 1.0f/5.0f, sunSize  * 1.0f/5.0f);
 		// mars
-		bodies[4].transform.localScale = new Vector3(sunSize * 1.0f/208.0f, sunSize * 1.0f/208.0f, sunSize * 1.0f/208.0f);
+		bodies[4].transform.localScale = new Vector3(sunSize * 1.0f/5.0f, sunSize * 1.0f/5.0f, sunSize * 1.0f/5.0f);
 		// jupiter
-		bodies[5].transform.localScale = new Vector3(sunSize * 1.0f/9.68f, sunSize * 1.0f/9.68f, sunSize * 1.0f/9.68f);
+		bodies[5].transform.localScale = new Vector3(sunSize * 1.0f/5.68f, sunSize * 1.0f/9.68f, sunSize * 1.0f/9.68f);
 		// saturn
 		bodies[6].transform.localScale = new Vector3(sunSize * 1.0f/11.4f, sunSize * 1.0f/11.4f, sunSize * 1.0f/11.4f);
 		// uranus
@@ -162,7 +227,10 @@ public class Planet_Data_Loader : MonoBehaviour {
 		// neptune 
 		bodies[8].transform.localScale = new Vector3(sunSize * 1.0f/27.7f, sunSize * 1.0f/27.7f, sunSize * 1.0f/27.7f);
 		// pluto
-		bodies[9].transform.localScale = new Vector3(sunSize * 1.0f/625.0f, sunSize * 1.0f/625.0f, sunSize * 1.0f/625.0f);	
+		bodies[9].transform.localScale = new Vector3(sunSize * 1.0f/5.0f, sunSize * 1.0f/5.0f, sunSize * 1.0f/5.0f);	
 	}
+
+
+
 }
 
