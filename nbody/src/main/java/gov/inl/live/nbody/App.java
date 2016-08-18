fac6adf7dbc1a1ca257a09b2436b4531c93ff6cd
Dr. James H. Money <james.money@inl.gov>
2016-08-18 10:43:46 -0600

    LIVE-14 NBody SQL generator
    Built NBody SQL generator code for storing 1 AU of data at a time. Added unit tests for polynomial class.

---
+++ nbody/src/main/java/gov/inl/live/nbody/App.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gov.inl.live.nbody;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author monejh
+ */
+public class App
+{
+    public static void main(String[] args) throws FileNotFoundException 
+    {
+        //NBody nbody = new NBody();
+        //nbody.run();
+        NBody2 nbody = new NBody2();
+        nbody.run();
+//        NBodySQLGenerator generator = new NBodySQLGenerator();
+//        generator.run();
+    }
+    
+}
