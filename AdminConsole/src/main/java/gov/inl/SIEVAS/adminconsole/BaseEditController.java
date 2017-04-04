ea2acd34a4fa1c3a7e6a7337ec14bf768a28a103
Dr. James H. Money <james.money@inl.gov>
2017-04-04 17:07:32 -0600

    SIEVAS-25 Add user and validation
    Added user info (still in progress) and validation.

--- AdminConsole/src/main/java/gov/inl/SIEVAS/adminconsole/BaseEditController.java
+++ AdminConsole/src/main/java/gov/inl/SIEVAS/adminconsole/BaseEditController.java
@@ -8,16 +8,25 @@ package gov.inl.SIEVAS.adminconsole;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
 import org.apache.http.client.methods.HttpPost;
@@ -34,7 +43,7 @@ public abstract class BaseEditController<T extends IIdentifier> implements Initi
 {
     private static final String CONTENT_TYPE_HEADER = "Content-Type";
     private static final String CONTENT_TYPE_JSON = "application/json";
-    private static final String DEFAULT_ENCODING = "UTF-8";
+    protected static final String DEFAULT_ENCODING = "UTF-8";
     protected final String DATA_SAVING_ERROR_MSG = "Save " + getObjectTextName() + " error";
     private static final String COLON = ":";
     
@@ -43,6 +52,11 @@ public abstract class BaseEditController<T extends IIdentifier> implements Initi
     private final RestController restController = RestController.getInstance();
     protected T object;
     private boolean createMode = false;
+    
+    private HashMap<Control,Boolean> validatedFields = new HashMap<>();
+    private HashMap<Control,Callback<Control,Boolean>> validationFunctions = new HashMap<>();
+    private List<Control> fieldsToValidate = new ArrayList<Control>();
+    private HashMap<Control,ChangeListener<Boolean>> changeListeners = new HashMap<>();
     
     /* Need these two buttons */
     @FXML
@@ -67,6 +81,8 @@ public abstract class BaseEditController<T extends IIdentifier> implements Initi
     
     /***
      * Updates this.object with values from the form fields.
+     * @return True if updated successfully, false otherwise. False typically 
+     *          means validation failed.
      */
     public abstract void UpdateObjectWithFormData();
     
@@ -123,8 +139,23 @@ public abstract class BaseEditController<T extends IIdentifier> implements Initi
     private boolean SaveData()
     {
         String url = baseURL + getObjectURL();
-        if (!createMode)
-            url += object.getId();
+        try
+        {
+            if (!createMode)
+                url += URLEncoder.encode(object.getId().toString(),DEFAULT_ENCODING);
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(DATA_SAVING_ERROR_MSG);
+            alert.setHeaderText(DATA_SAVING_ERROR_MSG);
+
+            alert.setContentText(e.getMessage());
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, e);
+            
+            alert.showAndWait();
+            return false;
+        }
         
         
         HttpEntityEnclosingRequestBase request;
@@ -208,9 +239,94 @@ public abstract class BaseEditController<T extends IIdentifier> implements Initi
     private void onSave(ActionEvent event)
     {
         Stage stage = (Stage) btnCancel.getScene().getWindow();
+        boolean result = validateFields();
+        if (!result)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Validation Error");
+            alert.setHeaderText("Check fields");
+            alert.setContentText("Please check all fields before submitting!");
+            alert.showAndWait();
+
+            return;
+        }
         UpdateObjectWithFormData();
-        boolean result = SaveData();
         if (result)
-            stage.close();
+        {
+            result = SaveData();
+            if (result)
+                stage.close();
+            else
+                return;
+        }
+        else
+            return;
+        
+    }
+    
+    public void AddControlToValidate(Control control, Callback<Control,Boolean> callback)
+    {
+        validatedFields.put(control, Boolean.FALSE);
+        validationFunctions.put(control, callback);
+        fieldsToValidate.add(control);
+        
+        ChangeListener<Boolean> listener = new ChangeListener<Boolean>(){
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue)
+            {
+                if (!newValue) //lost focus
+                    validatedFields.put(control, callback.call(control));
+            }
+            
+        };
+        control.focusedProperty().addListener(listener);
+        changeListeners.put(control, listener);
+    }
+    
+    public void RemoveControlToValidate(Control control)
+    {
+        if (validatedFields.containsKey(control))
+            validatedFields.remove(control);
+        if (validationFunctions.containsKey(control))
+            validationFunctions.remove(control);
+        fieldsToValidate.remove(control);
+        
+        if (changeListeners.containsKey(control))
+        {
+            control.focusedProperty().removeListener(changeListeners.get(control));
+            changeListeners.remove(control);
+        }
+    }
+    
+    protected boolean validateFields()
+    {
+        //first validate fields
+        boolean valid = true;
+        for(Control field: fieldsToValidate)
+        {
+            if (validatedFields.containsKey(field))
+            {
+                if (validatedFields.get(field) == Boolean.FALSE)
+                {
+                    if (validationFunctions.containsKey(field))
+                    {
+                        Callback<Control,Boolean> callback = validationFunctions.get(field);
+                        if (callback!=null)
+                        {
+                            boolean result = callback.call(field);
+                            validatedFields.put(field,result);
+                            if (!result)
+                                valid = false;
+                        }
+                            
+                    }
+                    else
+                        valid = false;
+                }
+            }
+            else
+                valid = false;
+        }
+        return valid;
     }
 }
--- AdminConsole/src/main/java/gov/inl/SIEVAS/adminconsole/user/UserController.java
+++ AdminConsole/src/main/java/gov/inl/SIEVAS/adminconsole/user/UserController.java
@@ -10,10 +10,18 @@ import gov.inl.SIEVAS.adminconsole.BaseController;
 import gov.inl.SIEVAS.adminconsole.JsonListResult;
 import gov.inl.SIEVAS.adminconsole.permissiongroup.PermissionGroup;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -22,7 +30,42 @@ import javafx.scene.control.MenuItem;
  */
 public class UserController extends BaseController<UserInfo>//implements Initializable
 {
+    @FXML
+    private TableColumn colEnabled;
     
+    @FXML
+    private TableColumn colLocked;
+    
+    @FXML
+    private TableColumn colExpired;
+    
+    @Override
+    public void initialize()
+    {
+        super.initialize();
+        Callback<TableColumn<UserInfo, Boolean>, TableCell<UserInfo, Boolean>> cellFactory = new Callback<TableColumn<UserInfo, Boolean>, TableCell<UserInfo, Boolean>>() {
+
+            public TableCell<UserInfo, Boolean> call(TableColumn<UserInfo, Boolean> p) {
+
+                return new CheckBoxTableCell<UserInfo, Boolean>(new Callback<Integer, ObservableValue<Boolean>>(){
+                    @Override
+                    public ObservableValue<Boolean> call(Integer param)
+                    {
+                        
+                    return new SimpleBooleanProperty(p.getCellData(param).booleanValue());
+                    }
+                    
+                });
+                }
+            };
+        colEnabled.setCellFactory(cellFactory);
+        colExpired.setCellFactory(cellFactory);
+        colLocked.setCellFactory(cellFactory);
+
+            
+
+        
+    }
     
 
     @Override
@@ -34,7 +77,16 @@ public class UserController extends BaseController<UserInfo>//implements Initial
     @Override
     public UserInfo createObject()
     {
-        return new UserInfo();
+        UserInfo user = new UserInfo();
+        user.setFirstName("");
+        user.setLastName("");
+        user.setEnabled(true);
+        user.setPassword("");
+        user.setPassword2("");
+        user.setUsername("");
+        user.setPermissionGroupCollection(new ArrayList<PermissionGroup>());
+        
+        return user;
     }
 
     @Override
@@ -60,6 +112,9 @@ public class UserController extends BaseController<UserInfo>//implements Initial
     {
         return new TypeReference<JsonListResult<UserInfo > >(){};
     }
+    
+    
+    
     
     
     
