04344f937c06e93dff798abe6ed6cb860bab38fb
Dr. James H. Money <james.money@inl.gov>
2017-07-19 13:11:31 -0600

    SIEVAS-55 Update for open source release
    Fixed nbody driver generation.

---
+++ SolarSystemViewer/Assets/scripts/SIEVASSession.cs
@@ -0,0 +1,89 @@
+﻿/***
+ * 
+
+Copyright 2017 Idaho National Laboratory.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+***/
+
+
+using System;
+
+
+[Serializable]
+public class SIEVASSession
+{
+	public SIEVASSession ()
+	{
+	}
+
+	public long id {
+		get;
+		set;
+	}
+
+	public string name {
+		get;
+		set;
+	}
+
+	public string dataStreamName
+	{
+		get;
+		set;
+	}
+
+	public string controlStreamName
+	{
+		get;
+		set;
+	}
+
+	public String activemqUrl
+	{
+		get;
+		set;
+	}
+
+	/*public object owner
+	{
+		get;
+		set;
+	}
+
+	public object users
+	{
+		get;
+		set;
+	}
+
+	public object groups
+	{
+		get;
+		set;
+	}
+
+	public string typeName
+	{
+		get;
+		set;
+	}*/
+
+	public override string ToString ()
+	{
+		return string.Format ("[SIEVASSession: id={0}, name={1}, dataStreamName={2}, controlStreamName={3}, activemqUrl={4}]", id, name, dataStreamName, controlStreamName, activemqUrl);
+	}
+}
+
+
